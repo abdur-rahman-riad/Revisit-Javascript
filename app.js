2f3a4ad0c6bf2ea485c85b5e139be171240c377b
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-08-18 19:53:30 +0600

    Shopping Cart Complete

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
-function updateCaseNumber(isIncreasing) {
-    const caseInput = document.getElementById('case-number');
-    let caseNumber = parseInt(caseInput.value);
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = parseInt(productInput.value);
     if (isIncreasing == true) {
-        caseNumber = caseNumber + 1;
+        productNumber = productNumber + 1;
     }
-    else if (caseNumber > 0) {
-        caseNumber = caseNumber - 1;
+    else if (productNumber > 0) {
+        productNumber = productNumber - 1;
     }
-    caseInput.value = caseNumber;
-    //Update Case Total
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    productInput.value = productNumber;
+    //Update Product Total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+    calculateTotal();
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+// Total Price Calculation
+function calculateTotal() {
+
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
 }
 
 // Phone Increse Decrese Event Handle
 document.getElementById('phone-plus').addEventListener('click', function () {
-    console.log("Phone Plus Clicked");
+    updateProductNumber('phone', 1219, true);
 });
 document.getElementById('phone-minus').addEventListener('click', function () {
-    console.log("Phone Minus Clicked");
+    updateProductNumber('phone', 1219, false);
 });
 
 // Case Increse Decrese Event Handle
 document.getElementById('case-plus').addEventListener('click', function () {
-    updateCaseNumber(true);
+    updateProductNumber('case', 59, true);
 });
 document.getElementById('case-minus').addEventListener('click', function () {
-    updateCaseNumber(false);
+    updateProductNumber('case', 59, false);
 });
